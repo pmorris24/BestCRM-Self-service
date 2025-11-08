3bd04e7e41cbd9676886f135b1c636692f64d2ae
pmorris24 <patrickryanmorris23@gmail.com>
2025-11-07 23:25:25 -0500

    Delete src directory

--- src/components/MemoizedDashboardWidget.tsx
+++
@@ -1,9 +0,0 @@
-// src/components/MemoizedDashboardWidget.tsx
-import React from 'react';
-import { DashboardWidget, DashboardWidgetProps } from '@sisense/sdk-ui';
-
-export const MemoizedDashboardWidget = React.memo(
-  (props: DashboardWidgetProps) => {
-    return <DashboardWidget {...props} />;
-  }
-);
